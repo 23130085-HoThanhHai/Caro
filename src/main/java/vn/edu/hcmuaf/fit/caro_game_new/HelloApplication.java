c38b8f3d6d1f2d0c781989dbc6482fd8195a5003
23130085-HoThanhHai <23130085@st.hcmuaf.edu.vn>
2026-05-29 18:18:30 +0700

    create new repo

---
+++ src/main/java/vn/edu/hcmuaf/fit/caro_game_new/HelloApplication.java
@@ -0,0 +1,9 @@
+package vn.edu.hcmuaf.fit.caro_game_new;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/vn/edu/hcmuaf/fit/caro_game_new/HelloResource.java
@@ -0,0 +1,14 @@
+package vn.edu.hcmuaf.fit.caro_game_new;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
